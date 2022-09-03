7dd3d4cdc42eb4e022a2d0b017974961bc0c31f9
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-03 11:45:14 -0700

    Js learnings

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let date;
 setInterval(() => {
     
 a = new Date();
-date = a.getDate()
+date = a.toLocaleDateString()
 time = a.getHours()+ ':' +a.getMinutes()+ ':' +a.getSeconds();
-document.getElementById('time').innerHTML = time + ":"+ date;
+document.getElementById('time').innerHTML = time + " on "+ date;
 }, 1000);
